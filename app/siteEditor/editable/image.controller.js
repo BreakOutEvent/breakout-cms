9550c0fbbd9136940c32be5d64001ef196a4e4d5
Lukas Heddendorp <lu.heddendorp@gmail.com>
2016-04-28 22:01:27 +0200

    Fix the image posting endpoint

--- app/siteEditor/editable/image.controller.js
+++ app/siteEditor/editable/image.controller.js
@@ -2,12 +2,13 @@
  * Created by l.heddendorp on 20.03.2016.
  */
 export default class SettingsCtrl {
-  constructor ($mdDialog, $mdToast, Upload, $timeout) {
+  constructor ($mdDialog, $mdToast, Upload, $timeout, API_URL) {
     'ngInject';
     this._dialog = $mdDialog;
     this._mdToast = $mdToast.showSimple;
     this._upload = Upload;
     this._timeout = $timeout;
+    this.API_URL = API_URL;
   }
   cancel(){
     this._dialog.cancel();
@@ -15,7 +16,7 @@ export default class SettingsCtrl {
   uploadFile(file) {
     if (file) {
       file.upload = this._upload.upload({
-        url: 'api/image',
+        url: API_URL+'/api/image',
         data: { image: file }
       });
 
